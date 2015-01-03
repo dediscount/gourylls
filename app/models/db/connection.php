4df2e0e23405ed0ebde135d6e6a815b64785b295
Blueswing <qq429515154@gmail.com>
2015-01-03 17:10:47 +0100

    update

--- app/models/db/connection.php
+++ app/models/db/connection.php
@@ -5,10 +5,15 @@ $password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
-?>
+ //echo "Connected successfully";
+ require_once 'utility.php';
+ $sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
