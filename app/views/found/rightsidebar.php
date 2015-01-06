a95054e74b4a68ae89d0105a3a32d83fe69ad7eb
Blueswing <qq429515154@gmail.com>
2015-01-06 23:39:44 +0100

    sql

--- app/views/found/rightsidebar.php
+++ app/views/found/rightsidebar.php
@@ -1,12 +1,22 @@
 <!--login navigator on right side-->
 <div class="navbar  pull-right navbar-login">
-	<!--display when not logined-->
-	<a class="login">
-		<i class="glyphicon glyphicon-user" data-toggle="modal" href="#loginModal"><span> Log in</span></i>
-	</a>
-	
-	<!--display when logined-->
-	<a class="logined">
-		<img src="../user/userid/userid_icon.png" class="img-circle navbar-user-icon" title="Go to my page">
-	</a>
+    <?php
+    if (isset($_SESSION["account"])) {
+        
+        ?>
+    <!--display when logined-->
+        <a class="logined">
+            <img src="/gourylls/public/user/userid/userid_icon.png" class="img-circle navbar-user-icon" title="Go to my page">
+        </a>
+        
+        <?php
+    } else {
+        ?>
+        <!--display when not logined-->
+        <a class="login">
+            <i class="glyphicon glyphicon-user" data-toggle="modal" href="#loginModal"><span> Log in</span></i>
+        </a>
+    <?php
+}
+?>
 </div>
