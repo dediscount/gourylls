4d8de994dbfd5f1747c008834a1d3e390909bdbc
KarlBao <karl930219@gmail.com>
2015-01-10 21:30:26 +0100

    bug fixed

--- app/views/found/uploadModal.php
+++ app/views/found/uploadModal.php
@@ -5,19 +5,21 @@
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 				<h4 class="modal-title">Upload</h4>
 			</div>
-			<div class="modal-body">
-				<div>
-					<img id="upload-photo-preview" src=""/>
+			<form onsubmit="#" action="" method="POST" role="form">
+				<div class="modal-body">
+					<div>
+						<img id="upload-photo-preview" src=""/>
+					</div>
+					<div id="upload-photo-text" class="form-group">
+						<input id="upload-title" type="text" class="form-control" placeholder="Title" autofocus required="required"><!--get username from database-->
+					</div>
+					<textarea id="upload-description" class="form-control" rows="3" placeholder="description"></textarea>
 				</div>
-				<div id="upload-photo-text" class="form-group">
-					<input id="upload-title" type="text" class="form-control" placeholder="Title" autofocus required="required"><!--get username from database-->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary btn-hue">Save</button>
 				</div>
-				<textarea id="upload-description" class="form-control" rows="3" placeholder="description"></textarea>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary btn-hue">Save</button>
-			</div>
+			</form>
 		</div><!-- /.modal-content -->
 	</div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
